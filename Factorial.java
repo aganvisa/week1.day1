ff7a4da55ec67bf0d270b46eb05ee60ee192c14f
aganvisa <103848654+aganvisa@users.noreply.github.com>
2022-04-19 20:29:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class Factorial {
+	
+
+	public static void main(String[] args) {
+		int input=5;
+		int fact=1;
+		// TODO Auto-generated method stub
+		for(int i=1;i<=input;i++) {
+			fact=i*fact;
+			
+			 
+		}
+		System.out.println(fact);
+	}
+}
